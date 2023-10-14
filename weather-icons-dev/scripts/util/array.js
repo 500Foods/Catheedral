ef9d678457a73c8e4de4de117215abe6474dc009
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2023-10-13 17:24:14 -0700

    Delete weather-icons-dev directory

--- weather-icons-dev/scripts/util/array.js
+++
@@ -1,9 +0,0 @@
-export function chunkArray(array, chunkSize) {
-    const chunks = [];
-
-    for (let i = 0; i < array.length; i += chunkSize) {
-        chunks.push(array.slice(i, i + chunkSize));
-    }
-
-    return chunks;
-}
